630c1872f623304cf6ab5052050f5f22e31e68e3
Josh Keegan <josh.keegan@gmx.com>
2023-03-08 18:14:10 +0000

    fix #18 up & down not working (layer 0). Was a collision with the built in (un)maximize default keybinds.

--- extension.js
+++ extension.js
@@ -11,6 +11,13 @@ function enable() {
 
     this.settings = ExtensionUtils.getSettings("org.gnome.shell.extensions.sst");
 
+    // TODO: Don't disable if there's no keybind collision.
+    // TODO: Reset keybinds back to what they were beforehand, not their defaults (user may have
+    //  customised them).
+    // Ideally this should just remove the colliding keybind & re-add it when the extension is 
+    //  disabled. e.g. "unmaximize" defaults to ['<Super>Down', '<Alt>F5'] and there's no collision
+    //  on '<Alt>F5' so it could remain.
+
     // disable native tiling
     this._gnomeMutterSettings = ExtensionUtils.getSettings("org.gnome.mutter");
     this._gnomeMutterSettings.set_boolean("edge-tiling", false);
@@ -22,6 +29,12 @@ function enable() {
     this._gnomeMutterKeybindSettings = ExtensionUtils.getSettings("org.gnome.mutter.keybindings");
     this._gnomeMutterKeybindSettings.set_strv("toggle-tiled-left", []);
     this._gnomeMutterKeybindSettings.set_strv("toggle-tiled-right", []);
+
+    // disable native fullscreen as we replace this with our own fullscreen call if moving a tiled 
+    //  window up and there's no tile above it
+    this._gnomeKeybindSettings = ExtensionUtils.getSettings("org.gnome.desktop.wm.keybindings");
+    this._gnomeKeybindSettings.set_strv("maximize", []);
+    this._gnomeKeybindSettings.set_strv("unmaximize", []);
 
     this.tiles = new Tiles.Tiles();
     this.mouseHandler = new MouseHandler();
@@ -45,6 +58,10 @@ function disable() {
     // re-enable native tiling keybinds
     this._gnomeMutterKeybindSettings.reset("toggle-tiled-left");
     this._gnomeMutterKeybindSettings.reset("toggle-tiled-right");
+
+    // re-enable native fullscreen keybinds
+    this._gnomeKeybindSettings.reset("maximize");
+    this._gnomeKeybindSettings.reset("unmaximize");
 }
 
 function init() {
