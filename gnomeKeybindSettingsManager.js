ba22939061685965eadbb68ea11eb0143e7e87a0
Josh Keegan <josh.keegan@gmx.com>
2023-03-08 19:47:28 +0000

    Revert all GNOME settings to their original values rather than defaults

--- gnomeKeybindSettingsManager.js
+++ gnomeKeybindSettingsManager.js
@@ -6,59 +6,113 @@ var Manager = class GnomeKeybindSettingsManager {
     constructor(sstSettings) {
         this._resetFns = [];
 
-        // disable native tiling
-        this._gnomeMutterSettings = ExtensionUtils.getSettings("org.gnome.mutter");
-        this._gnomeMutterSettings.set_boolean("edge-tiling", false);
-        this._gnomeShellSettings = ExtensionUtils.getSettings("org.gnome.shell.overrides");
-        this._gnomeShellSettings.set_boolean("edge-tiling", false);
+        // Fail gracefully: failure to update one setting shouldn't leave all of the others
+        //  in a modified state
+        try {
+            // disable native tiling
+            this._resetFns.push(
+                this.settingsSetBool(ExtensionUtils.getSettings("org.gnome.mutter"),
+                    "edge-tiling", false));
+            this._resetFns.push(
+                this.settingsSetBool(ExtensionUtils.getSettings("org.gnome.shell.overrides"),
+                    "edge-tiling", false));
 
-        // disable native tiling keybindings as edge-tiling being off doesn't disable
-        //  it via keybinds for some reason.
-        this._gnomeMutterKeybindSettings = ExtensionUtils.getSettings("org.gnome.mutter.keybindings");
-        this._gnomeMutterKeybindSettings.set_strv("toggle-tiled-left", []);
-        this._gnomeMutterKeybindSettings.set_strv("toggle-tiled-right", []);
+            // disable native tiling keybindings as edge-tiling being off doesn't disable
+            //  it via keybinds for some reason.
+            const gnomeMutterKeybinds = ExtensionUtils.getSettings("org.gnome.mutter.keybindings");
+            this._resetFns.push(
+                this.settingsSetStrv(gnomeMutterKeybinds, "toggle-tiled-left", []));
+            this._resetFns.push(
+                this.settingsSetStrv(gnomeMutterKeybinds, "toggle-tiled-right", []));  
 
-        // Native fullscreen keybinds can collide with tile layer 0 up & down, make our keybinds take
-        //  precedence since you can also make windows fullscreen via ours if there is no tile above the window.
-        // Retain keybinds that we don't collide with though (e.g. unmaximize also has <Alt>F5 as a default that we can keep)
-        const gnomeKeybinds = ExtensionUtils.getSettings("org.gnome.desktop.wm.keybindings");
-        this._resetFns.push(
-            this.removeValueFromSettingsArray(
-                gnomeKeybinds, "maximize", sstSettings.get_strv("tile-move-up-layer-0")));
-        this._resetFns.push(
-            this.removeValueFromSettingsArray(
-                gnomeKeybinds, "unmaximize", sstSettings.get_strv("tile-move-down-layer-0")));
+            // Native fullscreen keybinds can collide with tile layer 0 up & down, make our keybinds take
+            //  precedence since you can also make windows fullscreen via ours if there is no tile above the window.
+            // Retain keybinds that we don't collide with though (e.g. unmaximize also has <Alt>F5 as a default that we can keep)
+            const gnomeKeybinds = ExtensionUtils.getSettings("org.gnome.desktop.wm.keybindings");
+            this._resetFns.push(
+                this.settingsRemoveFromStrv(
+                    gnomeKeybinds, "maximize", sstSettings.get_strv("tile-move-up-layer-0")));
+            this._resetFns.push(
+                this.settingsRemoveFromStrv(
+                    gnomeKeybinds, "unmaximize", sstSettings.get_strv("tile-move-down-layer-0")));
+        }
+        catch (e) {
+            logError(e, "Failed to set a GNOME setting, resetting any that have already been set");
+            this.destroy();
+
+            throw new Error("Failed to update GNOME settings");
+        }
     }
 
     destroy() {
-        // re-enable native tiling
-        this._gnomeMutterSettings.reset("edge-tiling");
-        this._gnomeShellSettings.reset("edge-tiling");
-
-        // re-enable native tiling keybinds
-        this._gnomeMutterKeybindSettings.reset("toggle-tiled-left");
-        this._gnomeMutterKeybindSettings.reset("toggle-tiled-right");
-
-        this._resetFns.forEach(f => f());
+        this._resetFns.forEach(f => {
+            // Fail gracefully: failure to reset one setting shouldn't prevent others from being reset
+            try {
+                f();
+            }
+            catch (e) {
+                logError(e, "Failed to reset a GNOME setting");
+            }
+        });
         this._resetFns = null;
     }
 
     /**
-     * 
+     * Updates a strv (string array) setting, removing the specified values
      * @param {Gio.Settings} settings - a settings object loaded for a schema 
      * @param string key - key within the settings schema to update (must correlate with a string array type strv)
      * @param string[] values - values within the array to remove
      * @returns function to return key back to its original state
      */
-    removeValueFromSettingsArray(settings, key, values) {
+    settingsRemoveFromStrv(settings, key, values) {
         const orig = settings.get_strv(key);
         const removed = orig.filter(s => !values.includes(s));
         log(`Removing '${values}' from ${key} setting. Original '${orig}', now '${removed}'`);
         settings.set_strv(key, removed);
 
-        return function() {
-            log(`Returning setting ${key} back to its original value '${orig}'`);
+        return () => {
+            this.logRevertSetting(key, orig);
             settings.set_strv(key, orig);
         }
+    }
+
+    /**
+     * Updates a bool setting to the specified value
+     * @param {Gio.Settings} settings - a settings object loaded for a schema 
+     * @param string key - key within the settings schema to update
+     * @param bool value 
+     * @returns function to return key back to its original state
+     */
+    settingsSetBool(settings, key, value) {
+        const orig = settings.get_boolean(key);
+        log(`Updating ${key} setting. Original ${orig}, now ${value}`);
+        settings.set_boolean(key, value);
+
+        return () => {
+            this.logRevertSetting(key, orig);
+            settings.set_boolean(key, orig);
+        }
+    }
+
+    /**
+     * Updates a strv (string array) setting to the specified value
+     * @param {Gio.Settings} settings - a settings object loaded for a schema 
+     * @param string key - key within the settings schema to update
+     * @param string[] value
+     * @returns function to return key back to its original state 
+     */
+    settingsSetStrv(settings, key, value) {
+        const orig = settings.get_strv(key);
+        log(`Updating ${key} setting. Original '${orig}', now '${value}'`);
+        settings.set_strv(key, value);
+
+        return () => {
+            this.logRevertSetting(key, orig);
+            settings.set_strv(key, orig);
+        }
+    }
+
+    logRevertSetting(key, orig) {
+        log(`Returning setting ${key} back to its original value '${orig}'`);
     }
 }
